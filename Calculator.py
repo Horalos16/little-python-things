95f26c08bb2d49b0309e14af8d186210b8f69d1d
Ondrej Horalik <ondro.1216@gmail.com>
2023-09-27 11:59:56 -0700

    I created a calculator. I added -, *, /,

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,19 @@
 
-print("Alright I am your calculator, give me two numbers. \n")
+print("Alright I am your calculator, give me first number, an operator and second number. \n")
 first_number = int(input())
+operator = input()
 second_number = int(input())
 result = first_number + second_number
-print("The result is ", result, ".")
+result_1 = first_number - second_number
+result_2 = first_number * second_number
+result_3 = first_number / second_number
+if(operator == "-"):    
+	print(first_number, "-", second_number, "=", result_1)
+elif(operator == "+"):
+    print(first_number, "+", second_number, "=", result)
+elif(operator == "*"):
+    print(first_number, "*", second_number, "=", result_2)
+elif(operator == "/"):
+	print(first_number, "/", second_number, "=", result_3)
+else:
+	print("Sorry bro these are not numbers.")
