eb0277c0f207a6e2f81a17e685d39102fe9b6602
Ondrej Horalik <ondro.1216@gmail.com>
2023-09-27 11:21:30 -0700

    I created a calculator

---
+++ Calculator.py
@@ -0,0 +1,6 @@
+
+print("Alright I am your calculator, give me two numbers. \n")
+first_number = int(input())
+second_number = int(input())
+result = first_number + second_number
+print("The result is ", result, ".")
