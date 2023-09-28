777ad53180d5d9c9a718a2ac04b26aa6480c315e
Ondrej Horalik <ondro.1216@gmail.com>
2023-09-28 03:29:21 -0700

    I made a program that can tell me my shopping list. :)

---
+++ shopping_list.py
@@ -0,0 +1,9 @@
+print("Give me things you wanna buy.")
+item_1 = input()
+item_2 = input()
+item_3 = input()
+shopping_list = []
+shopping_list.append(item_1)
+shopping_list.append(item_2)
+shopping_list.append(item_3)
+print("You want to buy ", shopping_list, ".")
