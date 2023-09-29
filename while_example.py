72a1c1ab7bbb1d73beb7e4f87d2d8b625e6ee791
Ondrej Horalik <ondro.1216@gmail.com>
2023-09-29 10:56:08 -0700

    I added colors to my program>:)

--- while_example.py
+++ while_example.py
@@ -1,8 +1,12 @@
 import time
+from colorama import init
+from termcolor import colored
+
+init()
 greeting = ""
 while(True):
 	greeting = greeting + "ПРИВЕТ "
-	print(greeting)	
+	print(colored(greeting, "black", "on_yellow"))
 	if(len(greeting)>135):
 		greeting = ""		
 	time.sleep(0.1)
