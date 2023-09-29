a63f7a6b5750c09d46af8e305f96bdc69d7f74dd
Ondrej Horalik <ondro.1216@gmail.com>
2023-09-29 11:33:41 -0700

    I made a reaction game:)

--- while_example.py
+++ while_example.py
@@ -1,12 +1,27 @@
-import time
+import time 
 from colorama import init
 from termcolor import colored
 
-init()
-greeting = ""
-while(True):
-	greeting = greeting + "ПРИВЕТ "
-	print(colored(greeting, "black", "on_yellow"))
-	if(len(greeting)>135):
-		greeting = ""		
-	time.sleep(0.1)
+def color_drawing():
+	
+	init()
+	greeting = ""
+	while(True):
+		greeting = greeting + "ПРИВЕТ "
+		print(colored(greeting, "black", "on_yellow"))
+		if(len(greeting)>135):
+			greeting = ""		
+		time.sleep(0.1)
+
+def drawing_limited(): 
+
+	counter = 0
+
+	while(counter < 1000581):
+		print(counter)
+		counter = counter+1
+
+
+# ===== main ======
+drawing_limited()
+#color_drawing()
