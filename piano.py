72f958e2d3725e686d043ded4f7b3d4dfbf914b5
Ondrej Horalik <ondro.1216@gmail.com>
2023-10-09 11:10:17 -0700

    If you press A it will make a4 sound. If you press keys it will  make sound:)

--- piano.py
+++ piano.py
@@ -1,12 +1,17 @@
 import keyboard
 import time
 import winsound
-frequency =440 # Set Frequency To 2500 Hertz
 duration = 150 # Set Duration To 1000 ms == 1 second
-
+print("Alright press keys, A, S, D, F, G,  and have fun :D")
 while(True):
 	time.sleep(0.1)
 	if(keyboard.is_pressed("a")):
-		winsound.Beep(frequency, duration)
+		winsound.Beep(440, duration)
 	elif(keyboard.is_pressed("s")):		
+		winsound.Beep(494, duration)
+	elif(keyboard.is_pressed("d")):
 		winsound.Beep(523, duration)
+	elif(keyboard.is_pressed("f")):
+		winsound.Beep(659, duration)
+	elif(keyboard.is_pressed("g")):
+		winsound.Beep(698, duration)
