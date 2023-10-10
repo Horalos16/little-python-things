14400bf72cb390d796aa0a10c9a9153836356459
Ondrej Horalik <ondro.1216@gmail.com>
2023-10-10 08:10:35 -0700

    I added more keys to make sounds.

--- piano.py
+++ piano.py
@@ -2,7 +2,7 @@ import keyboard
 import time
 import winsound
 duration = 150 # Set Duration To 1000 ms == 1 second
-print("Alright press keys, A, S, D, F, G,  and have fun :D")
+print("Alright press keys, A, S, D, F, G, H, J, K, L and have fun :D")
 while(True):
 	time.sleep(0.1)
 	if(keyboard.is_pressed("a")):
@@ -15,3 +15,11 @@ while(True):
 		winsound.Beep(659, duration)
 	elif(keyboard.is_pressed("g")):
 		winsound.Beep(698, duration)
+	elif(keyboard.is_pressed("h")):
+		winsound.Beep(736, duration)
+	elif(keyboard.is_pressed("j")):
+		winsound.Beep(775, duration)
+	elif(keyboard.is_pressed("k")):
+		winsound.Beep(812, duration)
+	elif(keyboard.is_pressed("l")):
+		winsound.Beep(852, duration)
