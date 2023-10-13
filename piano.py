bbdf3b5876ebd63a9d3745a7a09819afa2eb0c76
Ondrej Horalik <ondro.1216@gmail.com>
2023-10-13 12:03:08 -0700

    If you press 1 or 2 it will make random sounds

--- piano.py
+++ piano.py
@@ -1,8 +1,10 @@
 import keyboard
 import time
 import winsound
+import random
 duration = 150 # Set Duration To 1000 ms == 1 second
 print("Alright press keys, A, S, D, F, G, H, J, K, L and have fun :D")
+print("Or press 1 or 2 to have even more fun (+_+) .")
 while(True):
 	time.sleep(0.1)
 	if(keyboard.is_pressed("a")):
@@ -10,7 +12,7 @@ while(True):
 	elif(keyboard.is_pressed("s")):		
 		winsound.Beep(494, duration)
 	elif(keyboard.is_pressed("d")):
-		winsound.Beep(523, duration)
+		winsound.Beep(37, duration)
 	elif(keyboard.is_pressed("f")):
 		winsound.Beep(659, duration)
 	elif(keyboard.is_pressed("g")):
@@ -23,3 +25,11 @@ while(True):
 		winsound.Beep(812, duration)
 	elif(keyboard.is_pressed("l")):
 		winsound.Beep(852, duration)
+	elif(keyboard.is_pressed("1")):
+		frequency = random.randint(37,1000) 
+		winsound.Beep(frequency, duration)
+	elif(keyboard.is_pressed("2")):
+		for counter in range(20):
+			frequency = random.randint(37,1000)
+			print("Playing "+str(frequency)) 
+			winsound.Beep(frequency, duration)
