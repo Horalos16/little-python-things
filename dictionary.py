4caed093abc2a8fa7be202654264da3875ac5fb1
Ondrej Horalik <ondro.1216@gmail.com>
2023-10-22 02:00:01 -0700

    I made czech and english dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,11 @@
+DICT = {"kocka":"cat",
+        "pes":"dog",
+        "mys":"mouse",
+        "zralok":"shark"}
+
 czech_word = input()
-if(czech_word == "kocka"):
-	print("Kocka is a cat")
-elif(czech_word == "pes"):
-	print("Pes is a dog")
-elif(czech_word == "mys"):
-	print("Mys is a mouse")
+if czech_word in DICT.keys():
+	english_word = DICT[czech_word]
+	print(czech_word + " is a " + english_word)
 else:
 	print("I dont know that word bro.")
