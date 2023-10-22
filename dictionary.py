381687e3096797d3ff9501350832cbdee600a67c
Ondrej Horalik <ondro.1216@gmail.com>
2023-10-22 01:51:29 -0700

    I made czech and english dictionary

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+czech_word = input()
+if(czech_word == "kocka"):
+	print("Kocka is a cat")
+elif(czech_word == "pes"):
+	print("Pes is a dog")
+elif(czech_word == "mys"):
+	print("Mys is a mouse")
+else:
+	print("I dont know that word bro.")
