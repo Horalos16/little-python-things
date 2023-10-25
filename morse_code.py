6c24b57352b00f1299ea5382d6e7fc94337a67ed
Ondrej Horalik <ondro.1216@gmail.com>
2023-10-25 10:03:55 -0700

    I made this programm to translate words to morse code (+_+)

--- morse_code.py
+++ morse_code.py
@@ -1,4 +1,35 @@
 MORSE_CODE = {"a" : ".-",
               "b" : "-...",
-              "c" : "-.-."}
-print(MORSE_CODE)
+              "c" : "-.-.",
+              "d" : "-..", 
+              "e" : ".",
+              "f" : "..-.",
+              "g" : ".-.",
+              "h" : "....",
+              "ch" : "----",
+              "i" : "..",
+              "j" : ".---",
+              "k" : "-.-",
+              "l" : ".-..",
+              "m" : "--",
+              "n" : "-.",
+              "o" : "---",
+              "p" : ".--.",
+              "q" : "--.-",
+              "r" : ".-.",
+              "s" : "...",
+              "t" : "-",
+              "u" : "..-",
+              "v" : "...-",
+              "w" : ".--",
+              "x" : "-..-",
+              "y" : "-.--",
+              "z" : "--..",}
+print("Give me a word so I can transalate it to morse code.")
+word = input()
+morse_message = ""
+for char in word:
+    morse_char = MORSE_CODE[char]
+    morse_message = morse_message + morse_char + "|"
+
+print(morse_message)
